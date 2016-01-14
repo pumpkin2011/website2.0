1e041fc333a573a3189b000e6515c15542b585e7
zhenhuanlee <zhenhuanlee@outlook.com>
2016-01-14 10:23:34 +0800

    backend admin:home admin:member

--- app.rb
+++ app.rb
@@ -3,7 +3,6 @@ require 'bundler/setup'
 require 'sinatra'
 require 'slim'
 require 'pry'
-require 'pry'
 require 'hiredis'
 require 'redis'
 
@@ -17,7 +16,49 @@ before do
 end
 
 get '/' do
-  # binding.pry
-  @content = $redis.get('b')
+	@home = $redis.hgetall('home')
   slim :index
+end
+
+get '/members' do
+	binding.pry
+	slim :members
+end
+
+# admin
+# index
+get '/admin/home' do
+	slim :admin_home, layout: :layout_admin
+end
+
+post '/admin/home' do
+	$redis.hmset("home",
+							 "image_url", params[:image_url], 
+							 "intro", params[:intro])
+	slim :admin_home, layout: :layout_admin
+end
+
+# ---------------
+
+# members
+get '/admin/members' do
+	slim :admin_members, layout: :layout_admin
+end
+
+post '/admin/members' do
+	id = $redis.incr("members.count")
+	$redis.hmset("member:#{id}",
+							 "id", id,
+							 "image_url", params[:member_image],
+							 "name", params[:member_name],
+							 "gender", params[:member_gender],
+							 "intro", params[:member_intro])
+	slim :admin_members, layout: :layout_admin
+end
+
+# helpers
+helpers do
+	def nav(str=nil)
+		request.path =~ /#{str||'index'}/ ? 'active' : '' 
+	end
 end
