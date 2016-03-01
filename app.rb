b9293a53d44e1bb10c367250c6edc528cc453425
zhenhuanlee <zhenhuanlee@outlook.com>
2016-03-01 15:38:06 +0800

    add papers module

--- app.rb
+++ app.rb
@@ -27,6 +27,14 @@ before do
 			@members << $redis.hgetall("member:#{id}")
 		end
 	end
+
+	# set @papers before visiting
+	if request.path =~ /papers/ then
+		@papers = []
+		$redis.zrange("paper:ids", 0, -1, withscores: true).map(&:first).each do |id|
+			@papers << $redis.hgetall("paper:#{id}")
+		end
+	end
 end
 
 # qiniu uptoken
@@ -44,6 +52,10 @@ end
 
 get '/members' do
 	slim :members, layout: :layout_front
+end
+
+get '/papers' do
+	slim :papers, layout: :layout_front
 end
 
 # admin
@@ -72,10 +84,15 @@ get '/admin/members' do
 	slim :admin_members
 end
 
+get '/admin/member/new' do
+	@member = {}
+	slim :admin_member
+end
+
 post '/admin/member' do
-	members = $redis.zrange("members:ids", 0, -1, withscores: true)
-	id   = (members.map(&:first).max || 1).to_i + 1
-	sort = (members.map(&:last).max || 1).to_i + 1
+	members = $redis.zrange("member:ids", 0, -1, withscores: true)
+	id   = (members.map(&:first).max || 0).to_i + 1
+	sort = (members.map(&:last).max || 0).to_i + 1
 	$redis.zadd("members:ids", sort, id)
 	$redis.hmset("member:#{id}",
 							 "id", id,
@@ -83,11 +100,6 @@ post '/admin/member' do
 							 "name", params[:member_name],
 							 "intro", params[:member_intro])
 	redirect('/admin/members')
-end
-
-get '/admin/member/new' do
-	@member = {}
-	slim :admin_member
 end
 
 get '/admin/member/edit/:id' do
@@ -109,6 +121,49 @@ get '/admin/member/delete/:id' do
 	$redis.zrem("members:ids", params[:id])
 	redirect('/admin/members')
 end
+
+# papers
+get '/admin/papers' do
+	slim :admin_papers
+end
+
+get '/admin/paper/new' do
+	@paper = {}
+	slim :admin_paper
+end
+
+post '/admin/paper' do
+	papers = $redis.zrange("paper:ids", 0, -1, withscore: true)
+	id   = (papers.map(&:first).max || 0) + 1
+	sort = (papers.map(&:last).max  || 0) + 1
+	$redis.zadd("paper:ids", sort, id)
+	$redis.zadd("paper:ids", sort, id)
+	$redis.hmset("paper:#{id}",
+							 "id", id,
+							 "date", params[:paper_date],
+							 "content", params[:paper_content])
+	redirect('/admin/papers')
+end
+
+get '/admin/paper/edit/:id' do
+	@paper = $redis.hgetall("paper:#{params[:id]}")
+	slim :admin_paper
+end
+
+put '/admin/member' do
+	$redis.hmset("paper:#{params[:paper_id]}",
+							 "id", params[:paper_id],
+							 "date", params[:paper_date],
+							 "intro", params[:paper_content])
+	redirect('/admin/papers')
+end
+
+get '/admin/paper/delete/:id' do
+	$redis.del("paper:#{params[:id]}")
+	$redis.zrem("paper:ids", params[:id])
+	redirect('/admin/papers')
+end
+
 
 # helpers
 helpers do
