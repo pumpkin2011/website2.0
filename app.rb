d94a0174340a6851f7409b01bface5f9824e9205
zhenhuanlee <zhenhuanlee@outlook.com>
2016-03-05 08:26:41 +0800

    change the way of saving members and papers

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require 'pry'
 require 'hiredis'
 require 'redis'
 require 'qiniu'
-# require 'json'
+require 'json'
 
 def setup_redis
   # uri = URI.parse('redis://127.0.0.1:6379') #10000
@@ -19,8 +19,20 @@ before do
 	#qiniu
 	Qiniu.establish_connection! :access_key => 'lodTWhr36v_H1tx_YKNuCq1kgn2JluMGsx5iytzd',
 															:secret_key => 'FmLqr6BK9x_-Y3ktPgdD1Ve8ng9PCnOvBnc752Et'
+	
+	# set @members and @papers before visiting index 
+	%w(member paper).each do |name|
+		plural_name = name + 's'
+		if request.path.include?(plural_name) then
+			instance_name = "@#{plural_name}"
+			ids = $redis.zrange("#{name}:ids", 0, -1, withscores: true).map(&:first)
+			values = $redis.hgetall(plural_name).values.map!{|item| eval item }
+			values = values.sort_by { |a| ids.index(a[:id].to_s) }
+			instance_variable_set(instance_name, values) 
+		end
+	end
 
-	# set @members before visiting members
+=begin
 	if request.path =~ /members/ then
 		@members = []
 		$redis.zrange("member:ids", 0, -1, withscores: true).map(&:first).each do|id|
@@ -35,6 +47,7 @@ before do
 			@papers << $redis.hgetall("paper:#{id}")
 		end
 	end
+=end
 end
 
 # qiniu uptoken
@@ -60,7 +73,6 @@ end
 
 # admin
 # index
-
 get '/admin' do
 	redirect('/admin/home')
 end
@@ -89,38 +101,25 @@ get '/admin/member/new' do
 	slim :admin_member
 end
 
+
 post '/admin/member' do
-	members = $redis.zrange("member:ids", 0, -1, withscores: true)
-	id   = (members.map(&:first).max || 0).to_i + 1
-	sort = (members.map(&:last).max || 0).to_i + 1
-	$redis.zadd("member:ids", sort, id)
-	$redis.hmset("member:#{id}",
-							 "id", id,
-							 "image_url", params[:member_image],
-							 "name", params[:member_name],
-							 "intro", params[:member_intro])
-	redirect('/admin/members')
+	create_or_update_record('member', %w(id image_url name intro))
 end
 
 get '/admin/member/edit/:id' do
-	@member = $redis.hgetall("member:#{params[:id]}")
+	@member = eval $redis.hget("members", "member:#{params[:id]}")
 	slim :admin_member
 end
 
 put '/admin/member' do
-	$redis.hmset("member:#{params[:member_id]}",
-							 "id", params[:member_id],
-							 "image_url", params[:member_image],
-							 "name", params[:member_name],
-							 "intro", params[:member_intro])
-	redirect('/admin/members')
+	create_or_update_record('member', %w(id image_url name intro))
 end
 
 get '/admin/member/delete/:id' do
-	$redis.del("member:#{params[:id]}")
-	$redis.zrem("member:ids", params[:id])
-	redirect('/admin/members')
+	delete_record('member', params[:id])
 end
+
+# -------------------
 
 # papers
 get '/admin/papers' do
@@ -133,37 +132,23 @@ get '/admin/paper/new' do
 end
 
 post '/admin/paper' do
-	papers = $redis.zrange("paper:ids", 0, -1, withscore: true)
-	id   = (papers.map(&:first).max || 0) + 1
-	sort = (papers.map(&:last).max  || 0) + 1
-	$redis.zadd("paper:ids", sort, id)
-	$redis.zadd("paper:ids", sort, id)
-	$redis.hmset("paper:#{id}",
-							 "id", id,
-							 "date", params[:paper_date],
-							 "content", params[:paper_content])
-	redirect('/admin/papers')
+	create_or_update_record('paper', %w(id date content))
 end
 
 get '/admin/paper/edit/:id' do
-	@paper = $redis.hgetall("paper:#{params[:id]}")
+	@paper = eval $redis.hget("papers", "paper:#{params[:id]}")
 	slim :admin_paper
 end
 
-put '/admin/member' do
-	$redis.hmset("paper:#{params[:paper_id]}",
-							 "id", params[:paper_id],
-							 "date", params[:paper_date],
-							 "intro", params[:paper_content])
-	redirect('/admin/papers')
+put '/admin/paper' do
+	create_or_update_record('paper', %w(id date content))
 end
 
 get '/admin/paper/delete/:id' do
-	$redis.del("paper:#{params[:id]}")
-	$redis.zrem("paper:ids", params[:id])
-	redirect('/admin/papers')
+	delete_record('paper', params[:id])
 end
 
+# -----------------
 
 # helpers
 helpers do
@@ -171,3 +156,31 @@ helpers do
 		request.path =~ /#{str}/ ? 'active' : ''
 	end
 end
+
+# --------------------
+
+private
+	# create a new record
+	def create_or_update_record(name, arr)
+		plural_name = "#{name}s"
+		ids  = $redis.zrange("#{name}:ids", 0, -1, withscores: true)
+		if params[:id].empty? then
+			id   = (ids.map(&:first).max || 0).to_i + 1
+			sort = (ids.map(&:last).max || 0).to_i + 1
+			$redis.zadd("#{name}:ids", sort, id)
+		end
+		values = arr.map{ |item| params[item.to_sym] }
+		data = arr.zip(values).to_h
+		data["id"] = Integer(params[:id]) rescue id
+		$redis.hmset(plural_name, "#{name}:#{data['id']}", data)
+		redirect("/admin/#{plural_name}")
+	end
+
+	# delete a record
+	def delete_record(name, id)
+		plural_name = "#{name}s"
+		$redis.hdel(plural_name, "#{name}:#{id}")
+		$redis.zrem("#{name}:ids", id)
+		redirect("/admin/#{plural_name}")
+	end
+
