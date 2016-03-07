b9f4afab5c8a3562514662a9e5e65ad03acd4dd8
zhenhuanlee <zhenhuanlee@outlook.com>
2016-03-07 10:57:00 +0800

    edit config.ru

--- config.ru
+++ config.ru
@@ -1,2 +1,13 @@
-require File.expand_path '../app.rb', __FILE__
-run app
+# require File.expand_path '../app.rb', __FILE__
+# run app
+
+require 'rubygems'  
+require 'sinatra'  
+  
+set :env,:development  
+disable :run  
+  
+require './app.rb'  
+  
+# 在Sinatra的示例文档中是这样的: run Sinatra.application,但这样会报错的,修改后如下,正确启动.  
+run Sinatra::Application  
---
+++ unicorn.rb
@@ -0,0 +1,16 @@
+@dir = "/Users/lee/Documents/sinatra/website2.0/"
+
+worker_processes 2
+working_directory @dir
+
+timeout 30
+
+# we will use this in our nginx.conf later
+listen "#{@dir}tmp/sockets/unicorn.sock", :backlog => 64
+
+# Set process id path
+pid "#{@dir}tmp/pids/unicorn.pid"
+
+# Set log file paths
+stderr_path "#{@dir}log/unicorn.stderr.log"
+stdout_path "#{@dir}log/unicorn.stdout.log"
