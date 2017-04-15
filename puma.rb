b09587eba6eec83f80a3443ece55837a00e64790
pumpkin <zhenhuanlee@outlook.com>
2017-04-15 11:53:10 +0800

    puma config

--- puma.rb
+++ puma.rb
@@ -1,6 +1,21 @@
 workers 1
 threads 1, 6
 
+app_dir = File.expand_path("../", __FILE__)
+shared_dir = "#{app_dir}"
+
+puts app_dir
+
+bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
+
+# Logging
+stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+
+# Set master PID and state locations
+pidfile "#{shared_dir}/tmp/pids/puma.pid"
+state_path "#{shared_dir}/tmp/pids/puma.state"
+activate_control_app
+
 # preload_app!
 
 # rackup      DefaultRackup
